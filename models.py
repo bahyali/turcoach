30faba37cbb08f8793681b03f85665b505f757b1
Bahy <35463814+bahyali@users.noreply.github.com>
2024-02-18 11:07:24 +0100

    add models

---
+++ models.py
@@ -0,0 +1,58 @@
+from enum import Enum
+
+from pymongo import MongoClient
+from bunnet import Document, init_bunnet, Link
+from config import settings
+
+
+class TurfTypes(str, Enum):
+    natural = "natural"
+    artificial = "artificial"
+    hybrid = "hybrid"
+
+
+class Pitch(Document):
+    name: str
+    location: str
+
+    turf_type: TurfTypes
+
+    condition_score: int = 10
+
+    # maintenance
+    can_be_maintained: bool = True
+    last_maintained: int = 0
+    next_maintenance: int = 0
+
+    # replacement
+    replacement_date: int = 0
+    must_be_replaced: bool = False
+
+    def get_score(self) -> int:
+        return self.condition_score
+
+    def set_score(self, new_score: int) -> None:
+        self.condition_score = new_score
+
+
+class MaintenanceStatuses(str, Enum):
+    scheduled = "scheduled"
+    rescheduled = "rescheduled"
+    done = "done"
+    canceled = "canceled"
+
+
+class Maintenance(Document):
+    pitch: Link[Pitch]
+    timestamp: int
+    status: MaintenanceStatuses = MaintenanceStatuses.scheduled
+
+
+# Bunnet uses Pymongo client under the hood
+client = MongoClient(settings.MONGO_CONNECTION_STRING)
+
+# Initialize bunnet with the Product document class
+init_bunnet(
+    database=client.db_name,
+    document_models=[Pitch, Maintenance],
+)
